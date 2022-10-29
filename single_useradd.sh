a609c1f112e58f333271fb1d180f906a6959ed84
jonasbrum <att@wefixit.inf.br>
2022-10-29 14:26:09 -0300

    Atualização Users

--- single_useradd.sh
+++ single_useradd.sh
@@ -2,16 +2,16 @@
 
 echo "Create Users"
 
-useradd guest14 -c "guest user" -s /bin/bash -m -p $(openssl passwd -crypt pass123)
-passwd guest14 -e
+useradd guest20 -c "guest user" -s /bin/bash -m -p $(openssl passwd -crypt pass123)
+passwd guest20 -e
 
-useradd guest15 -c "guest user" -s /bin/bash -m -p $(openssl passwd -crypt pass123)
-passwd guest15 -e
+useradd guest21 -c "guest user" -s /bin/bash -m -p $(openssl passwd -crypt pass123)
+passwd guest21 -e
 
-useradd guest16 -c "guest user" -s /bin/bash -m -p $(openssl passwd -crypt pass123)
-passwd guest16 -e
+useradd guest22 -c "guest user" -s /bin/bash -m -p $(openssl passwd -crypt pass123)
+passwd guest22 -e
 
-useradd guest17 -c "guest user" -s /bin/bash -m -p $(openssl passwd -crypt pass123)
-passwd guest17 -e
+useradd guest23 -c "guest user" -s /bin/bash -m -p $(openssl passwd -crypt pass123)
+passwd guest23-e
 
 echo "DONE..."
